5a9c3097bf96f8bc9d0be6bfed5d566267a1cf71
hwhhh <hahaha@naver.com>
2025-07-25 17:37:49 +0900

    7.25 ticket

---
+++ src/ch2_control_starement/MultiIf02.java
@@ -0,0 +1,33 @@
+package ch2_control_starement;
+
+public class MultiIf02 {
+    public static void main(String[] args) {
+
+        int age = 10;
+        double ticket = 1000.0 ;
+        double discount ; // 할인율
+
+        String person;
+
+        if(age < 8){
+        discount = 1.0;
+        person = "유아(무료 입장)";
+        }else if(age<14){
+         discount = 0.5;
+         person = "어린이(50% 할인)";
+        }else if(age<20){
+            discount = 0.3;
+            person = "청소년(30% 할인)";
+        }else if(age>=65){
+            discount = 0.4;
+            person = "노인(40% 할인)";
+        }else{
+            discount = 0;
+            person = "성인(정가)";
+        }
+        String message = age + "살 :" + person;
+        System.out.println(message);
+        message = "가격은 "+ (int)(ticket*(1- discount)) + "원 입니다.";
+        System.out.println(message);
+    }
+}
